24f87bf975a4fae383190f289960f776141d61cf
Roshan-Gawande <166796833+Roshan-Gawande@users.noreply.github.com>
2026-01-29 15:50:18 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "google" {
   project     = var.project_id
   region      = var.region
-  credentials = file("${var.credentials}")
+  credentials = var.credentials != null ? file(var.credentials) : null
 }
